052d19b57dc259194252c814f23d905572a7ffeb
Jak Charlton <jakcharlton@gmail.com>
2012-06-21 09:02:01 +1000

    Refactored exception check

--- process_list.rb
+++ process_list.rb
@@ -4,7 +4,7 @@ class ProcessList
   end
 
   def add_process(process_id, resource_id, hold_wait)
-    raise ArgumentError, 'hold wait must be either :hold or :wait' if hold_wait != :hold and hold_wait != :wait
+    raise ArgumentError, 'hold wait must be either :hold or :wait' unless [:hold, :wait].include? hold_wait
 
     hold_wait == :hold ? @graph.add_node(process_id, resource_id) : @graph.add_node(resource_id, process_id)
   end
