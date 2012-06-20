ad91a7e63b97deea7c325d1e71071529b445b2a0
Jak Charlton <jakcharlton@gmail.com>
2012-06-21 08:57:55 +1000

    Refactor to ternary

--- process_list.rb
+++ process_list.rb
@@ -6,11 +6,7 @@ class ProcessList
   def add_process(process_id, resource_id, hold_wait)
     raise ArgumentError, 'hold wait must be either :hold or :wait' if hold_wait != :hold and hold_wait != :wait
 
-    if hold_wait == :hold
-      @graph.add_node(process_id, resource_id)
-    else
-      @graph.add_node(resource_id, process_id)
-    end
+    hold_wait == :hold ? @graph.add_node(process_id, resource_id) : @graph.add_node(resource_id, process_id)
   end
 
   def is_deadlocked?
